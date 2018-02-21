a622e127933e44a2598c746fe5c2eae33c44b37f
Guillermo Barrios del Valle <gbv@ier.unam.mx>
2018-02-21 16:35:54 -0600

    update files

--- modulos/Eplus.py
+++ modulos/Eplus.py
@@ -3,7 +3,7 @@ import time
 
 class readEP:
     def __init__(self,archivo,nombres):
-        self.datos  = pd.read_csv('datos/cubo.csv',sep=',',skiprows=[0],names=nombres)
+        self.datos  = pd.read_csv(archivo,sep=',',skiprows=[0],names=nombres)
         self.datos[nombres[0]] =self.datos[nombres[0]].str.replace(" ","",n=2)
         self.datos[nombres[0]] = self.datos[nombres[0]].str.replace(" ","/2017 ")
         self.datos[nombres[0]] = self.datos[nombres[0]].str.replace(" 24:00:00", " 23:59:59")
