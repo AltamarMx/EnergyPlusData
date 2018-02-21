48e4007a2e6380086dffcd723d6b6b73b46a562c
Guillermo Barrios del Valle <gbv@ier.unam.mx>
2018-02-21 17:58:39 -0600

    Add files via upload

--- modulos/Eplus.py
+++ modulos/Eplus.py
@@ -2,6 +2,29 @@ import pandas as pd
 import time
 
 class readEP:
+    """
+    Class to read csv files from an EnergyPlus Simulation and load data into a DataFrame.
+
+    Use:
+    readEP(arg1,arg2)
+
+    Parameters
+    ----------
+    arg1 : path of the file to load into a DataFrame
+        csv file containing date in the first column and variables in the followings.
+        arg1 = 'data/cubo.csv'
+    arg2 : list of names of the columns
+        Always the first column should be time, and the user should define the name each column. t is suggested for time.
+        arg2 = ['t','Ein','Eout', 'Nin','Nout', 'Oin','Oout', 'Sin','Sout', 'Pin','Pout','Tein','Teout']
+
+    Returns
+    -------
+    DataFrame
+        Returns a DataFrame with the date/time column as index for easy manipulation or visualization.
+
+    """
+    
+    
     def __init__(self,archivo,nombres):
         self.datos  = pd.read_csv(archivo,sep=',',skiprows=[0],names=nombres)
         self.datos[nombres[0]] =self.datos[nombres[0]].str.replace(" ","",n=2)
